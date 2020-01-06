a9747d3938177d0480a955f6474062930ad46d1d
Benjo <beniamino.tartarini@gmail.com>
2020-01-06 17:50:12 +0900

    Updates

---
+++ precache-manifest.d6e3c78be2470ee6094105bb7e9ee790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf09836315cc57f75c4b8f71245ed06",
+    "url": "/card-search-autocomplete/index.html"
+  },
+  {
+    "revision": "37fc0204a016124ac964",
+    "url": "/card-search-autocomplete/static/js/2.a9114bd6.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/card-search-autocomplete/static/js/2.a9114bd6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76e446c8fc3b0625467c",
+    "url": "/card-search-autocomplete/static/js/main.96a10f50.chunk.js"
+  },
+  {
+    "revision": "ecacefab07de1571402f",
+    "url": "/card-search-autocomplete/static/js/runtime-main.a89baae4.js"
+  }
+]);
